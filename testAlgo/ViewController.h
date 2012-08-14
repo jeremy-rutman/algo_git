b91cd43aaf7701932780caeee319ec35a988944f
Itai David <Itai@Itais-MacBook-Air.local>
2012-08-14 18:04:42 +0300

    Initial Commit

---
+++ testAlgo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  testAlgo
+//
+//  Created by Itai David on 8/14/12.
+//  Copyright (c) 2012 itaidddd@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ testAlgo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  testAlgo
+//
+//  Created by Itai David on 8/14/12.
+//  Copyright (c) 2012 itaidddd@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
