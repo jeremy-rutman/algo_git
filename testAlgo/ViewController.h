44f4b5752db393f6d0f528fbaa369f221675b8e6
Lior Neu-ner <Lior@Liors-MacBook-Pro.local>
2013-05-09 12:27:15 +0300

    bla

--- testAlgo/ViewController.h
+++ testAlgo/ViewController.h
@@ -81,7 +81,7 @@ Boolean AGps_state;
 -(void)writeStringToFile:(NSString*)aString;
 //Method writes a string to a text file
 +(void) writeToTextFile;
-+(void) writeToLogFile:(NSString*)aString;
+-(void) writeToLogFile:(NSString*)aString;
 -(void) integrateAcceleration:(AlgoVector*)aVector Nsamples:(int)N_samples;
 
 //+(void) displayContent;
